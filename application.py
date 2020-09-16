faddd4db24fda46dc9046fc2bd4c92b21529f978
BOBO-LU <luwenkai0820@gmail.com>
2020-09-16 09:55:40 +0800

    add pyodbc flask_cors into requirements.txt

--- application.py
+++ application.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_cors import CORS
+from sqltest import insert
+from config import DefaultConfig
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
+
+
+@app.route('/p', methods=['POST'])
+def connectDB():
+    print('connect db')
+    # print(request.data)
+    return "connect db"
