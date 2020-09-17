9f553aa1d1b68617e6c54f74bc68c009eff5ac5b
BOBO-LU <luwenkai0820@gmail.com>
2020-09-17 15:33:47 +0800

    CLEAN

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template
-from flask_cors import CORS
-from sqltest import insert, insert_test
-from config import DefaultConfig
-import os
+# from flask_cors import CORS
+# from sqltest import insert, insert_test
+# from config import DefaultConfig
+# import os
 app = Flask(__name__)
 
 
